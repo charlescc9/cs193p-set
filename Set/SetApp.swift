3d424f5da1ed38c7299a63caddd89251c7a0ad72
Charles Costello <charles@meno.local>
2025-01-20 16:27:10 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Charles Costello on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
